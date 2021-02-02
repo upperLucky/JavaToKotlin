8b02d1ddc7b5f32216e2e044baad13df1180538b
yunkun.wen <yunkun.wen@hstong.com>
2021-02-02 17:56:57 +0800

    xiufu

--- base/src/main/java/com/example/core/http/HttpClient.kt
+++ base/src/main/java/com/example/core/http/HttpClient.kt
@@ -35,8 +35,8 @@ class HttpClient : OkHttpClient {
 
 
                 override fun onResponse(call: Call, response: Response) {
-                    when (response.code()) {
-                        in 200..299 -> entityCallback.onSuccess(convert(response.body()?.string(), type))
+                    when (response.code) {
+                        in 200..299 -> entityCallback.onSuccess(convert(response.body?.string(), type))
                         in 400..499 -> entityCallback.onFailure("客户端错误")
                         in 500..599 -> entityCallback.onFailure("服务器错误")
                         else -> entityCallback.onFailure("未知错误")
